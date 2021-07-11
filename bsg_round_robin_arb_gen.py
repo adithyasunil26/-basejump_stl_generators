baf32fdb3000aa37d21d3521adaddbc2dc927d52
adithyasunil26 <adithyasunil26@gmail.com>
2021-07-11 06:12:51 +0400

    Corrected path and removed unwanted imports

--- bsg_round_robin_arb_gen.py
+++ bsg_round_robin_arb_gen.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 from fusesoc.capi2.generator import Generator
-import os
-import shutil
 import subprocess
-import tempfile
 
 class BSGRoundRobinArbGenerator(Generator):
     def run(self):
         channels = self.config.get('channels', 16)
         
-        args = ['python', 'bsg_round_robin_arb.py', str(channels)]
+        args = ['python', 'bsg_misc/bsg_round_robin_arb.py', str(channels)]
         
         cwd = self.files_root
         f=open("bsg_round_robin_arb.v","w")
