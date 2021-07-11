326d7b36efac956a23c539c3093eb3c6360043d3
adithyasunil26 <adithyasunil26@gmail.com>
2021-07-11 05:54:12 +0400

    Added round robin arbitration unit generator

---
+++ bsg_round_robin_arb_gen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from fusesoc.capi2.generator import Generator
+import os
+import shutil
+import subprocess
+import tempfile
+
+class BSGRoundRobinArbGenerator(Generator):
+    def run(self):
+        channels = self.config.get('channels', 16)
+        
+        args = ['python', 'bsg_round_robin_arb/bsg_round_robin_arb.py', str(channels)]
+        
+        cwd = self.files_root
+        f=open("bsg_round_robin_arb.v","w")
+        
+        rc = subprocess.call(args, cwd=cwd, stdout=f)
+        
+        if rc:
+            exit(1)
+        
+        self.add_files([{ 'bsg_round_robin_arb.v' : {'file_type' : 'verilogSource'}}])
+
+g = BSGRoundRobinArbGenerator()
+g.run()
+g.write()
